f9596a059239e202ee0963075ec4a5e676d892bb
AiYinSiChuan <1335630864@qq.com>
2023-05-28 16:30:26 +0800

    预编译头文件

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,3 +1,4 @@
+#include"hzpch.h"
 #include"Application.h"
 #include"Hazel/Events/ApplicationEvent.h"
 #include"Hazel/Log.h"
@@ -13,8 +14,8 @@ namespace Hazel
 	}
 	void Application::Run()
 	{
-		WindowResizeEvent e(1280, 720);
-		HZ_TRACE(e);
+		WindowResizeEvent e(1280, 720);//创建这个事件的实例
+		HZ_TRACE(e);//通过此宏打印出来，由于重载过右移运算符所以可以这样写
 		while (true)
 		{
 
---
+++ Hazel/src/hzpch.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+#include <filesystem>
+
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+
+#ifdef HZ_PLATFORM_WINDOWS
+	#include<Windows.h>
+
+#endif // DEBUG
