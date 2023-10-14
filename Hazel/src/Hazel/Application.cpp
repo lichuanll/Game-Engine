15ffa92207fb17b04edabc314e13cd019efeda60
AiYinSiChuan <1335630864@qq.com>
2023-10-14 16:32:31 +0800

    InputLoop

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -4,6 +4,7 @@
 #include"Hazel/Log.h"	
 #include<GLFW/glfw3.h>
 #include<glad/glad.h>
+#include "Input.h"
 namespace Hazel
 {
 #define BIND_EVENT_FN(x) std::bind(&Application::x,this,std::placeholders::_1)
@@ -56,6 +57,8 @@ namespace Hazel
 			
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+			auto [x, y] = Input::GetMousePosition();
+			HZ_CORE_TRACE("{0},{1}", x, y);
 			m_Window->OnUpdate();
 		}
 		WindowResizeEvent e(1280, 720);//创建这个事件的实例
---
+++ Hazel/src/Hazel/Platform/Windows/WindowsInput.cpp
@@ -0,0 +1,47 @@
+#include "hzpch.h"
+#include "WindowsInput.h"
+
+
+#include "GLFW/glfw3.h"
+#include "Hazel/Application.h"
+
+namespace Hazel
+{
+	Input* Input::s_Instance = new WindowsInput();
+	bool WindowsInput::IsKeyPressedImpl(int keycode)
+	{
+		auto window = static_cast<GLFWwindow*> (Application::Get().GetWindow().GetNativeWindow());
+		auto state= glfwGetKey(window,keycode);
+		return state == GLFW_PRESS || state == GLFW_REPEAT;
+	}
+
+	bool WindowsInput::IsMouseButtonPressedImpl(int button)
+	{
+		auto window = static_cast<GLFWwindow*> (Application::Get().GetWindow().GetNativeWindow());
+		auto state = glfwGetMouseButton(window, button);
+		return state == GLFW_PRESS;
+	}
+
+	float WindowsInput::GetMouseXImpl()
+	{
+		auto window = static_cast<GLFWwindow*> (Application::Get().GetWindow().GetNativeWindow());
+		double xPos, yPos;
+		glfwGetCursorPos(window, &xPos, &yPos);
+		return (float)xPos;
+	}
+
+	float WindowsInput::GetMouseYImpl()
+	{
+		auto window = static_cast<GLFWwindow*> (Application::Get().GetWindow().GetNativeWindow());
+		double xPos, yPos;
+		glfwGetCursorPos(window, &xPos, &yPos);
+		return (float)yPos;
+	}
+	std::pair<float, float> WindowsInput::GetMousePositionImpl()
+	{
+		auto window = static_cast<GLFWwindow*> (Application::Get().GetWindow().GetNativeWindow());
+		double xPos, yPos;
+		glfwGetCursorPos(window, &xPos, &yPos);
+		return {(float)xPos,(float)yPos};
+	}
+}
